0db02f5e034fdc24c003ec681cffed511ce61483
jaumetuset <indianajaume@gmail.com>
2024-02-12 10:20:41 +0100

    Tablero + Players

--- brandiq-front/src/app/components/rules/rules.component.ts
+++ brandiq-front/src/app/components/rules/rules.component.ts
@@ -1,21 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.css'
 })
+
 export class RulesComponent {
-  showModal: boolean = false;
 
-  openModal() {
-    this.showModal = !this.showModal;
-  }
-
-  closeModal() {
-    this.showModal = false;
-  }
 }
