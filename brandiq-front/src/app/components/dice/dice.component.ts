b6caa161a53d5ec76e7fccf502fbb58cd0bba3d3
HugoSanchis16 <hugolluissimarro03@gmail.com>
2024-02-13 01:32:39 +0100

    Css bastante cambiado, playerList, Dado, Header, Footer, Home, ranking, estadisticas, Login, registre

--- brandiq-front/src/app/components/dice/dice.component.ts
+++ brandiq-front/src/app/components/dice/dice.component.ts
@@ -5,24 +5,40 @@ import { Component, EventEmitter, Output } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './dice.component.html',
-  styleUrls: ['./dice.component.css']
+  styleUrls: ['./dice.component.css'],
 })
-
 export class DiceComponent {
-  
   @Output() diceRolled = new EventEmitter<number>();
 
   cubeTransform = 'translateY(400px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
   time = 2;
 
   rollDice() {
-    this.cubeTransform = 'translateY(400px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
-    setTimeout(() => {
-      const randomValue = this.getRandomNumber(1, 6);
-      const finalRotation = this.getRotationForNumber(randomValue);
-      this.cubeTransform = `translateY(400px) ${finalRotation}`;
-      console.log(randomValue);
-      this.diceRolled.emit(randomValue); 
+    this.cubeTransform =
+      'translateY(400px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
+
+    // Aumentar el ángulo de rotación más veces antes de llegar al valor final
+    let totalRotations = 10; // Puedes ajustar este valor según tus necesidades
+    let currentRotation = 0;
+
+    const rotateInterval = setInterval(() => {
+      // Incrementar el ángulo de rotación
+      currentRotation += 360;
+
+      // Aplicar la nueva transformación con el ángulo actualizado
+      this.cubeTransform = `translateY(400px) rotateX(0deg) rotateY(${currentRotation}deg) rotateZ(0deg)`;
+
+      // Verificar si se han completado todas las rotaciones
+      if (currentRotation >= totalRotations * 360) {
+        clearInterval(rotateInterval);
+
+        // Después de las rotaciones, realizar la lógica original
+        const randomValue = this.getRandomNumber(1, 6);
+        const finalRotation = this.getRotationForNumber(randomValue);
+        this.cubeTransform = `translateY(400px) ${finalRotation}`;
+        console.log(randomValue);
+        this.diceRolled.emit(randomValue);
+      }
     }, 100);
   }
 
@@ -37,7 +53,7 @@ export class DiceComponent {
       3: 'rotateY(90deg)',
       4: 'rotateY(-90deg)',
       5: 'rotateX(-90deg)',
-      6: 'rotateX(180deg)'
+      6: 'rotateX(180deg)',
     };
 
     return rotations[number] || 'rotateZ(0deg)';
