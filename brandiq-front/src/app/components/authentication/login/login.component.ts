e07c35c03c2e5488d58d25a19f70158f0157f1d5
HugoSanchis16 <hugolluissimarro03@gmail.com>
2024-02-22 01:58:48 +0100

    PROYECTO VERSION FINAL DEFINITIVA

--- brandiq-front/src/app/components/authentication/login/login.component.ts
+++ brandiq-front/src/app/components/authentication/login/login.component.ts
@@ -1,27 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, CommonModule],
+  imports: [RouterLink, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  nickname: string = '';
-  password: string = '';
+export class LoginComponent{
+
+  loginForm!: FormGroup;
   errorMessage: string = '';
-  constructor(private usersService: UsersService, private router: Router) {}
+
+  constructor(private usersService: UsersService, private router: Router, private formBuilder: FormBuilder) {
+    this.createLoginForm();
+  }
+  
+createLoginForm() {
+  
+  this.loginForm = this.formBuilder.group(
+    {
+    nickname: [
+      '', 
+      [
+      Validators.required, 
+      Validators.minLength(5),
+      Validators.pattern('.*[a-zA-Z].*'),
+      ]
+    ],
+    password: [
+      '', 
+      [
+        Validators.required,
+        Validators.minLength(8)
+      ],
+    ],
+  })
+}
 
   login() {
-    const user = { nickname: this.nickname, password: this.password };
+    if (this.loginForm.valid) {
+    const user = {
+      nickname: this.loginForm.value.nickname,
+      password: this.loginForm.value.password,
+    };
 
     // Utiliza el servicio para realizar la solicitud HTTP y manejar la lógica
-    let logged = this.usersService.login(user).subscribe({
+    this.usersService.login(user).subscribe({
       next: (data) => {
         console.log('Respuesta del servidor:', data);
         this.router.navigate(['/']);
@@ -33,4 +62,14 @@ export class LoginComponent {
       },
     });
   }
+  }
+  isInvalidAndTouched(controlName: string): boolean {
+    const control = this.loginForm.get(controlName);
+    return control!.invalid && (control!.touched || control!.dirty);
+  }
+
+  isNullOrWhiteSpace(value: string | null): boolean {
+    return value === null || value.trim() === '';
+  }
+
 }
